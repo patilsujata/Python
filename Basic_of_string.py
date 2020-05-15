ad841bef77c234e77b958e6dea0d515c53052886
Sujata S. Patil <36949072+patilsujata@users.noreply.github.com>
2020-05-15 23:42:54 +0530

    Add files via upload

---
+++ Basic_of_string.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Basics & Strings
+
+
+# In[9]:
+
+
+# Addition
+print(2+1)
+
+# Subtraction
+print(2-1)
+
+# Multiplication
+print(2*2)
+
+# Division
+print(3/2)
+
+# Specifying one of the numbers as a float
+print(3.0/2)
+
+# Works for either number
+print(3/2.0)
+
+# We can use this float() function to cast integers as floats:
+print(float(3)/2)
+
+
+# In[10]:
+
+
+2**3
+
+
+# In[11]:
+
+
+print(4**0.5)
+print(2 + 10 * 10 + 3)
+print((2+10) * (10+3))
+
+
+# In[13]:
+
+
+#Variable Assignments
+# Let's create an object called "a" and assign it the number 5
+a = 5
+
+
+# In[14]:
+
+
+a
+
+
+# In[15]:
+
+
+# Adding the objects
+a+a
+
+
+# In[16]:
+
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income*tax_rate
+# Show my taxes!
+my_taxes
+
+
+# In[21]:
+
+
+#Strings
+#Creating a String
+'hello'
+'This is also a string'
+"String built with double quotes"
+
+
+# In[19]:
+
+
+' I'm using single quotes, but will create an error'
+
+
+# In[22]:
+
+
+"Now I'm ready to use the single quotes inside a string!"
+
+
+# In[23]:
+
+
+'Mera Bharath Mahaan'
+
+
+# In[24]:
+
+
+"M'era B'harath M'ahaan"
+
+
+# In[25]:
+
+
+print('Mera Bharath Mahaan 1')
+print('Mera Bharath Mahaan 2')
+
+
+# In[28]:
+
+
+print('Mera Bharath Mahaan 1')
+print('Mera Bharath Mahaan 2')
+print('Use \n to print a new line')
+print('\n')
+print('See what I mean?')
+
+
+# In[29]:
+
+
+print("Use \\n to print a new line")
+
+
+# In[ ]:
+
+
+
+
